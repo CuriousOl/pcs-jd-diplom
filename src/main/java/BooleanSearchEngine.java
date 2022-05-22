72248fa6dff3705c89dbc85d73a0726df5d08a0e
Popova Olesya <popovaolesya87@gmail.com>
2022-05-22 15:28:03 +0300

    add .giignore

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,73 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    Map<String, List<PageEntry>> results = new TreeMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+
+        if (!pdfsDir.isDirectory()) return;
+
+        var listFiles = pdfsDir.listFiles();
+        if (listFiles == null) {
+            System.out.println("Файлов нет");
+            return;
+        }
+
+        for (File pdf : listFiles) {
+            if (pdf.isDirectory()) continue;
+
+            //для файлов:
+            var doc = new PdfDocument(new PdfReader(pdf));
+            for (int numberPage = 1; numberPage <= doc.getNumberOfPages(); ++numberPage) {
+
+                PdfPage page = doc.getPage(numberPage);
+                String text = PdfTextExtractor.getTextFromPage(page);
+                String[] words = text.split("\\P{IsAlphabetic}+");
+
+                //подсчитываем частоту повторения каждого слова в тексте текущего файла
+                Map<String, Integer> freqs = wordsFrequency(words);
+
+                //добавляем результаты поиска каждого слова в results
+                for (String key : freqs.keySet()) {
+                    if (results.containsKey(key)) {
+                        List<PageEntry> list = new ArrayList<>(results.get(key));
+//                        list = results.get(key);
+                        list.add(new PageEntry(pdf.getName(), numberPage, freqs.get(key)));
+                        results.replace(key, list);
+                    } else {
+                        results.put(key, List.of(new PageEntry(pdf.getName(), numberPage, freqs.get(key))));
+                    }
+                }
+            }
+        }
+
     }
 
-    @Override
+    public Map<String, Integer> wordsFrequency(String[] words) {
+        Map<String, Integer> freqs = new HashMap<>();
+        for (String word : words) {
+            if (word.isEmpty()) {
+                continue;
+            }
+            freqs.put(word.toLowerCase(), freqs.getOrDefault(word.toLowerCase(), 0) + 1);
+        }
+        return freqs;
+    }
+
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+
+        if (!results.containsKey(word)) {
+            return Collections.emptyList();
+        }
+        return results.get(word).stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,45 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pcs-jd-diplom/pdfs"));
+        //тесты
+//        System.out.println(engine.search("бизнес"));
+//        System.out.println(engine.search("skills"));
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        try (ServerSocket serverSocket = new ServerSocket(8989)) {
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream())
+                ) {
+                    // обработка одного подключения
+                    out.println("Пожалуйста, введите искомое слово и нажмите Enter");
+                    final String word = in.readLine();
+                    List<PageEntry> response = engine.search(word);
+                    System.out.println(listToJson(response));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
+    }
+
+    public static String listToJson(List<PageEntry> objects) {
+        Type listType = new TypeToken<List<PageEntry>>() {
+        }.getType();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String json = gson.toJson(objects, listType);
+        return json;
     }
 }
